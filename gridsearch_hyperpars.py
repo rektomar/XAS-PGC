c5a74204ad99ad31c5c0902af87902a29b7d03df
mlnpapez <mlnpapez@gmail.com>
2024-11-27 15:13:29 +0100

    Gridsearch (hyper-parameter templates)

--- gridsearch_hyperpars.py
+++ gridsearch_hyperpars.py
@@ -4,38 +4,75 @@ import itertools
 from utils.templates_hyperpars import *
 
 
-def grid_backend_btree(
+def grid_btree(
         nl = [2, 3],
-        nr = [1],
         ns = [20, 40],
         ni = [20, 40]
 ):
-    grid = itertools.product(nl, nr, ns, ni)
+    return [template_btree(*p) for p in list(itertools.product(nl, ns, ni))]
 
-    return [backend_btree(*p) for p in list(grid)]
+def grid_vtree(
+        nl = [2, 3],
+        ns = [20, 40],
+        ni = [20, 40]
+):
+    return [template_vtree(*p) for p in list(itertools.product(nl, ns, ni))]
+
+def grid_rtree(
+        nl = [2, 3],
+        nr = [1, 10],
+        ns = [20, 40],
+        ni = [20, 40]
+):
+    return [template_rtree(*p) for p in list(itertools.product(nl, nr, ns, ni))]
+
+def grid_ptree(
+        nl = [2, 3],
+        ns = [20, 40],
+        ni = [20, 40]
+):
+    return [template_ptree(*p) for p in list(itertools.product(nl, ns, ni))]
+
+def grid_ctree(
+        nh = [2, 3]
+):
+    return [template_ctree(p) for p in nh]
 
 
 def grid_zero_sort(dataset):
     order = ['unordered', 'canonical', 'bft', 'dft', 'rcm', 'rand']
-    backend_x_name = ['btree', 'btree']
-    backend_x_hpar = [
+    nc = [1, 8, 16]
+    backend_name = ['btree', 'vtree', 'rtree', 'ptree', 'ctree']
+    backend_grid = [grid_btree, grid_vtree, grid_rtree, grid_ptree, grid_ctree]
+    backend_xpar = [
+        {"nl":[3, 4],           "ns":[20, 40], "ni":[20, 40]},
+        {"nl":[3, 4],           "ns":[20, 40], "ni":[20, 40]},
         {"nl":[3, 4], "nr":[1], "ns":[20, 40], "ni":[20, 40]},
-        {"nl":[3, 4], "nr":[1], "ns":[20, 40], "ni":[20, 40]}]
-    backend_x_grid = [grid_backend_btree, grid_backend_btree]
-    backend_a_name = ['btree', 'btree']
-    backend_a_hpar = [
-        {"nl":[5, 6], "nr":[1], "ns":[20, 40], "ni":[20, 40]},
-        {"nl":[5, 6], "nr":[1], "ns":[20, 40], "ni":[20, 40]}]
-    backend_a_grid = [grid_backend_btree, grid_backend_btree]
+        {"nl":[3, 4],           "ns":[20, 40], "ni":[20, 40]},
+        {"nh":[32, 64]}
+    ]
+    backend_apar = [
+        {"nl":[5, 6],           "ns":[20, 40], "ni":[20, 40]},
+        {"nl":[3, 4],           "ns":[20, 40], "ni":[20, 40]},
+        {"nl":[3, 4], "nr":[1], "ns":[20, 40], "ni":[20, 40]},
+        {"nl":[3, 4],           "ns":[20, 40], "ni":[20, 40]},
+        {"nh":[32, 64]}
+    ]
+    backend_nr = [
+        [None],
+        [None],
+        [None],
+        [1, 10],
+        [None]
+    ]
     batch_size = [256]
     lr = [0.05]
     seed = [0, 1, 2, 3, 4]
 
     hyperpars = []
-    for bx_name, bx_grid, bx_hpar in zip(backend_x_name, backend_x_grid, backend_x_hpar):
-        for ba_name, ba_grid, ba_hpar in zip(backend_a_name, backend_a_grid, backend_a_hpar):
-            grid = itertools.product(order, [bx_name], bx_grid(**bx_hpar), [ba_name], ba_grid(**ba_hpar), batch_size, lr, seed)
-            hyperpars.extend([template_zero_sort(dataset, *p) for p in list(grid)])
+    for b_name, b_grid, b_xpar, b_apar, b_nr in zip(backend_name, backend_grid, backend_xpar, backend_apar, backend_nr):
+        grid = itertools.product(order, nc, b_nr, [b_name], b_grid(**b_xpar), b_grid(**b_apar), batch_size, lr, seed)
+        hyperpars.extend([template_zero_sort(dataset, *p) for p in list(grid)])
 
     return hyperpars
 
@@ -47,5 +84,5 @@ GRIDS = {
 
 if __name__ == "__main__":
     print(len(grid_zero_sort('qm9')))
-    # for p in grid_zero_sort('qm9'):
-    #     print(json.dumps(p, indent=4))
+    for p in grid_zero_sort('qm9')[-9:]:
+        print(json.dumps(p, indent=4))
--- utils/templates_hyperpars.py
+++ utils/templates_hyperpars.py
@@ -6,55 +6,53 @@ from utils.datasets import MOLECULAR_DATASETS
 NUM_EPOCHS = 40
 
 
-def backend_btree(
-        nl: int = 4,
-        nr: int = 1,
-        ns: int = 40,
-        ni: int = 40
-):
-    return {
-    "nl": nl,
-    "nr": nr,
-    "ns": ns,
-    "ni": ni
-}
+def template_btree(nl: int=4,            ns: int=40, ni: int=40): return {"nl": nl,           "ns": ns, "ni": ni}
+def template_vtree(nl: int=4,            ns: int=40, ni: int=40): return {"nl": nl,           "ns": ns, "ni": ni}
+def template_rtree(nl: int=4, nr: int=1, ns: int=40, ni: int=40): return {"nl": nl, "nr": nr, "ns": ns, "ni": ni}
+def template_ptree(nl: int=4,            ns: int=40, ni: int=40): return {"nl": nl,           "ns": ns, "ni": ni}
+def template_ctree(nh: int=64): return {"nh": nh}
 
 
 def template_zero_sort(
-        dataset: str,
-        order: str = "canonical",
-        bx: str = "btree",
-        bx_hpars: dict = backend_btree(),
-        ba: str = "btree",
-        ba_hpars: dict = backend_btree(),
-        batch_size: int = 1000,
-        lr: float = 0.05,
-        seed: int = 0
+    dataset: str,
+    order: str = "canonical",
+    nc: int = 100,
+    nr: int = None,
+    backend: str = "btree",
+    bx_hpars: dict = template_btree(),
+    ba_hpars: dict = template_btree(),
+    batch_size: int = 1000,
+    lr: float = 0.05,
+    seed: int = 0
 ):
-    return {
-    "dataset": dataset,
-    "order": order,
-    "model": "zero_sort",
-    "model_hpars": {
-        "nc": 100,
-        "bx": bx,
-        "bx_hpars": bx_hpars,
-        "ba": ba,
-        "ba_hpars": ba_hpars,
-        "device": "cuda"
-    },
-    "optimizer": "adam",
-    "optimizer_hpars": {
-        "lr": lr,
-        "betas": [
-            0.9,
-            0.82
-        ]
-    },
-    "num_epochs": NUM_EPOCHS,
-    "batch_size": batch_size,
-    "seed": seed
-}
+    hpars = {
+        "dataset": dataset,
+        "order": order,
+        "model": "zero_sort",
+        "model_hpars": {
+            "nc": nc,
+            "backend": backend,
+            "bx_hpars": bx_hpars,
+            "ba_hpars": ba_hpars,
+            "device": "cuda"
+        },
+        "optimizer": "adam",
+        "optimizer_hpars": {
+            "lr": lr,
+            "betas": [
+                0.9,
+                0.82
+            ]
+        },
+        "num_epochs": NUM_EPOCHS,
+        "batch_size": batch_size,
+        "seed": seed
+    }
+
+    if nr is not None:
+        hpars["model_hpars"]["nr"] = nr
+
+    return hpars
 
 
 HYPERPARS_TEMPLATES = [
