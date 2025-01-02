2023ee78cabc492dcf03d12a7257a5e642cf892d
mlnpapez <mlnpapez@gmail.com>
2025-01-02 14:22:26 +0100

    Evaluation (tables)

--- gridsearch_evaluate.py
+++ gridsearch_evaluate.py
@@ -1,61 +1,115 @@
 import os
-from numpy import NaN
+import numpy as np
 import pandas as pd
 
-from tqdm import tqdm
 from pylatex import Document, Package, NoEscape
 
-MODEL_NAMES_TABLE = {
-    'graphspn_zero_none': 'GraphSPN: None',
-    'graphspn_zero_full': 'GraphSPN: Full',
-    'graphspn_zero_rand': 'GraphSPN: Rand',
-    'graphspn_zero_sort': 'GraphSPN: Sort',
-    'graphspn_zero_kary': 'GraphSPN: $k$-ary',
-    'graphspn_zero_free': 'GraphSPN: Ind',
+
+BACKEND_NAMES = {
+    'btree': 'BTree',
+    'vtree': 'VTree',
+    'rtree': 'RAT',
+    'ptree': 'RAT-S',
+    'ctree': 'HCLT'
 }
 
 IGNORE = [
-    'atom_list',
     'device',
     'seed',
-    'res_f_valid',
-    'res_t_valid',
-    'cor_t_valid',
-    'res_f_unique',
-    'cor_t_unique',
-    'res_t_unique',
-    'res_f_novel',
-    'res_t_novel',
-    'cor_t_novel',
-    'res_f_novel_abs',
-    'res_t_novel_abs',
-    'cor_t_novel_abs',
-    'res_f_score',
-    'res_t_score',
-    'cor_t_score',
-    'res_f_unique_abs',
-    'res_t_unique_abs',
-    'cor_t_unique_abs',
-    'nll_val_approx',
-    'nll_tst_approx',
+    'sam_valid',
+    'sam_unique',
+    'sam_unique_abs',
+    'sam_novel',
+    'sam_novel_abs',
+    'sam_score',
+    'sam_m_stab',
+    'sam_a_stab',
+    'sam_fcd_val',
+    'sam_kldiv_val',
+    'sam_nspdk_val',
+    'sam_fcd_tst',
+    'sam_kldiv_tst',
+    'sam_nspdk_tst',
+    'res_valid',
+    'res_unique',
+    'res_unique_abs',
+    'res_novel',
+    'res_novel_abs',
+    'res_score',
+    'res_m_stab',
+    'res_a_stab',
+    'res_fcd_val',
+    'res_kldiv_val',
+    'res_nspdk_val',
+    'res_fcd_tst',
+    'res_kldiv_tst',
+    'res_nspdk_tst',
+    'cor_valid',
+    'cor_unique',
+    'cor_unique_abs',
+    'cor_novel',
+    'cor_novel_abs',
+    'cor_score',
+    'cor_m_stab',
+    'cor_a_stab',
+    'cor_fcd_val',
+    'cor_kldiv_val',
+    'cor_nspdk_val',
+    'cor_fcd_tst',
+    'cor_kldiv_tst',
+    'cor_nspdk_tst',
+    'time_sam',
+    'time_res',
+    'time_cor',
     'num_params',
-    'file_path',
     ]
 
-COLUMN_WORKING_NAMES = ['model', 'validity', 'validitywocheck',    'uniqueness', 'novelty']
-COLUMN_DISPLAY_NAMES = ['Model', 'Validity', 'Validity w/o check', 'Uniqueness', 'Novelty']
-
+COLUMN_NAMES = [
+        'Model',       'Valid', 'NSPDK', 'FCD', 'Unique',   'Novel']
 def baseline_models_qm9():
-    data = [['GraphVAE', 'GVAE', 'CVAE', 'RVAE', 'GraphNVP', 'GRF', 'GraphAF', 'GraphDF', 'MoFlow', 'ModFlow'],
-            [      55.7,   60.2,   10.2,   96.6,       83.1,  84.5,     100.0,     100.0,    100.0,       NaN],
-            [       NaN,    NaN,    NaN,    NaN,        NaN,   NaN,      67.0,      82.7,     89.0,      99.1],
-            [      76.0,    9.3,   67.5,    NaN,       99.2,  66.0,      94.2,      97.6,     98.5,      99.3],
-            [      61.6,   80.9,   90.0,   95.5,       58.2,  58.6,      88.8,      98.1,     96.4,     100.0]]
-    return pd.DataFrame.from_dict({k:v for k, v, in zip(COLUMN_WORKING_NAMES, data)})
+    rows = [
+        ['GraphAF',      77.43,   0.020,  5.27,    88.64,     86.59],
+        ['GraphDF',      93.88,   0.064, 10.93,    98.58,     98.54],
+        ['MoFlow',       91.36,   0.017,  4.47,    98.65,     94.72],
+        ['EDP-GNN',      47.52,   0.005,  2.68,    99.25,     86.58],
+        ['GraphEBM',      8.22,   0.030,  6.14,    97.90,     97.01],
+        ['SPECTRE',      87.30,   0.163, 47.96,    35.70,     97.28],
+        ['GDSS',         95.72,   0.003,  2.90,    98.46,     86.27],
+        ['DiGress',      99.00,   0.005,  0.36,    96.66,     33.40],
+        ['GRAPHARM',     90.25,   0.002,  1.22,    95.62,     70.39]
+    ]
+    return pd.DataFrame(rows, columns=COLUMN_NAMES)
+def baseline_models_zinc250k():
+    rows = [
+        ['GraphAF',      68.47,   0.044, 16.02,    98.64,     100.0],
+        ['GraphDF',      90.61,   0.177, 33.55,    99.63,     100.0],
+        ['MoFlow',       63.11,   0.046, 20.93,    99.99,     100.0],
+        ['EDP-GNN',      82.97,   0.049, 16.74,    99.79,     100.0],
+        ['GraphEBM',      5.29,   0.212, 35.47,    98.79,     100.0],
+        ['SPECTRE',      90.20,   0.109, 18.44,    67.05,     100.0],
+        ['GDSS',         97.01,   0.019, 14.66,    99.64,     100.0],
+        ['DiGress',      91.02,   0.082, 23.06,    81.23,     100.0],
+        ['GRAPHARM',     88.23,   0.055, 16.26,    99.46,     100.0]
+    ]
+    return pd.DataFrame(rows, columns=COLUMN_NAMES)
 
+def highlight_top3(x, type='max'):
+    styles = np.array(len(x)*[None])
+    match type:
+        case 'min':
+            i = np.argsort(x)[:3]
+        case 'max':
+            i = np.argsort(x)[-3:][::-1]
+        case _:
+            os.error('Unsupported type.')
 
-def texable(x):
-    return [n.replace('_', '-') for n in x]
+    styles[i] = [
+        'color:{c1};textbf:--rwrap;',
+        'color:{c2};textbf:--rwrap;',
+        'color:{c3};textbf:--rwrap;'
+    ]
+
+    return styles
 
 def latexify_style(df, path, row_names=None, column_names=None, precision=2):
     if row_names is not None:
@@ -63,11 +117,23 @@ def latexify_style(df, path, row_names=None, column_names=None, precision=2):
     if column_names is not None:
         df.rename(columns={o: n for o, n in zip(df.columns, column_names)}, inplace=True)
 
-    s = df.style.highlight_max(subset=df.columns[1:], axis=0, props='color:{red};' 'textbf:--rwrap;')
+    subset_min = [('QM9','NSPDK'), ('QM9','FCD'), ('Zinc250k','NSPDK'), ('Zinc250k','FCD')]
+    subset_max = [('QM9','Valid'), ('QM9','Unique'), ('QM9','Novel'), ('Zinc250k','Valid'), ('Zinc250k','Unique'), ('Zinc250k','Novel')]
+
+    s = df.style
+    s = s.apply(highlight_top3, type='min', subset=subset_min)
+    s = s.apply(highlight_top3, type='max', subset=subset_max)
     s.hide()
     s.format(precision=precision)
-    # s.caption = 'Results.'
-    s.to_latex(path, hrules=True)
+    s.format(precision=3, subset=[('QM9','NSPDK'), ('Zinc250k','NSPDK')])
+    s.to_latex(path, hrules=True, multicol_align='c')
+
+    with open(path, 'r') as file_data:
+        lines = file_data.read().splitlines()
+        lines.insert(3, '\\cmidrule(lr){2-6}')
+        lines.insert(4, '\\cmidrule(lr){7-11}')
+        lines.insert(16, '\\midrule')
+        print('\n'.join(lines), file=open(path, 'w'))
 
 def latexify_table(r_name, w_name, clean_tex=True):
     with open(r_name) as f:
@@ -76,41 +142,50 @@ def latexify_table(r_name, w_name, clean_tex=True):
     doc = Document(documentclass='standalone', document_options=('preview'), geometry_options={'margin': '0cm'})
     doc.packages.append(Package('booktabs'))
     doc.packages.append(Package('xcolor', options='table'))
+    doc.packages.append(NoEscape(r'\definecolor{c1}{RGB}{27,158,119}'))
+    doc.packages.append(NoEscape(r'\definecolor{c2}{RGB}{117,112,179}'))
+    doc.packages.append(NoEscape(r'\definecolor{c3}{RGB}{217,95,2}'))
     doc.append(NoEscape(table))
     doc.generate_pdf(f'{w_name}', clean_tex=clean_tex)
 
 
-def find_best_models(dataset, names_models, evaluation_dir):
-    data_unsu = pd.DataFrame(0., index=range(len(names_models)), columns=COLUMN_WORKING_NAMES)
+def find_best_backends(evaluation_dir, dataset, model, backends):
+    d_frame = pd.DataFrame(0., index=range(len(backends.keys())), columns=COLUMN_NAMES)
+    path = evaluation_dir + f'{dataset}/{model}/'
 
-    for i, model in tqdm(enumerate(names_models), leave=False):
-        res_frames = []
-        path = evaluation_dir + f'{model}/'
-        for f in os.listdir(path):
-            data = pd.read_csv(path + f)
-            data['file_path'] = f.replace('.csv', '.pt')
-            res_frames.append(data)
+    for i, backend in enumerate(backends.keys()):
+        b_frame = pd.concat([pd.read_csv(path + f) for f in os.listdir(path) if backend in f])
+        g_frame = b_frame.groupby(list(filter(lambda x: x not in IGNORE, b_frame.columns)))
+        a_frame = g_frame.agg({'sam_nspdk_val': 'mean'})
+        f_frame = g_frame.get_group(a_frame['sam_nspdk_val'].idxmin())
 
-        cat_frames = pd.concat(res_frames)
-        grp_frames = cat_frames.groupby(list(filter(lambda x: x not in IGNORE, cat_frames.columns)))
-        agg_frames = grp_frames.agg({
-            'res_f_valid': 'mean',
-            'file_path': 'first'
-        })
-        best_frame = grp_frames.get_group(agg_frames['res_f_valid'].idxmax())
+        d_frame.loc[i] = [BACKEND_NAMES[backend],
+                          100*f_frame['sam_valid'].mean(),
+                              f_frame['sam_nspdk_tst'].mean(),
+                              f_frame['sam_fcd_tst'].mean(),
+                          100*f_frame['sam_unique'].mean(),
+                          100*f_frame['sam_novel'].mean()]
 
-        data_unsu.loc[i] = [MODEL_NAMES_TABLE[model], 100*best_frame['cor_t_valid'].mean(), 100*best_frame['res_f_valid'].mean(), 100*best_frame['res_f_unique'].mean(), 100*best_frame['res_f_novel'].mean()]
-
-    return data_unsu
+    return d_frame
 
 
 if __name__ == "__main__":
-    evaluation_dir = 'results/gridsearch/model_evaluation/metrics/qm9/'
-    models = os.listdir(evaluation_dir)
+    evaluation_dir = 'results/gridsearch/model_evaluation/metrics/'
 
-    baselines = baseline_models_qm9()
-    ourmodels = find_best_models('qm9', models, evaluation_dir)
-    allmodels = pd.concat([baselines, ourmodels], ignore_index=True)
+    baselines_qm9 = baseline_models_qm9()
+    ourmodels_qm9 = find_best_backends(evaluation_dir, 'qm9', 'zero_sort', BACKEND_NAMES)
+    allmodels_qm9 = pd.concat([baselines_qm9, ourmodels_qm9], ignore_index=True)
 
-    latexify_style(allmodels, 'qm9.tab', column_names=COLUMN_DISPLAY_NAMES)
-    latexify_table('qm9.tab', 'qm9')
+    baselines_zinc250k = baseline_models_zinc250k()
+    ourmodels_zinc250k = find_best_backends(evaluation_dir, 'zinc250k', 'zero_sort', BACKEND_NAMES)
+    allmodels_zinc250k = pd.concat([baselines_zinc250k, ourmodels_zinc250k], ignore_index=True)
+
+    allmodels = allmodels_qm9.merge(allmodels_zinc250k, how='left', on='Model', suffixes=('-x', '-y'))
+    allmodels.columns = COLUMN_NAMES + COLUMN_NAMES[1:]
+
+    columns = [('','Models')] + [('QM9', name) for name in COLUMN_NAMES[1:]] + [('Zinc250k', name) for name in COLUMN_NAMES[1:]]
+    allmodels.columns = pd.MultiIndex.from_tuples(columns)
+    allmodels.head()
+
+    latexify_style(allmodels, 'unconditional.tab')
+    latexify_table('unconditional.tab', 'unconditional')
